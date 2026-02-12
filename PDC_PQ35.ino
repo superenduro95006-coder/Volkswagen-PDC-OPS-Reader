5e2ea790825a6d1dfe559755fffe61c6e18080df
superenduro95006-coder <superenduro95006@gmail.com>
2026-02-12 14:48:25 +0100

    Update PDC_PQ35.ino
    change messages according PQ35 dbc and extended logging to 8channel and status

--- PDC_PQ35.ino
+++ PDC_PQ35.ino
@@ -1,28 +1,28 @@
-// DBC File https://github.com/commaai/opendbc/blob/master/opendbc/dbc/vw_pq.dbc
+// DBC File https://github.com/superenduro95006-coder/Volkswagen-PDC-OPS-Reader/blob/main/docs/PQ35_46_ICAN_V3_6_9_F_20081104_ASR_V1_2.dbc
 
 #include <SPI.h>
 #include <mcp_can.h>
 
-// ---------------- Hardware ----------------
+// ---------------- Hardware ---------------- Arduino Micro (Leonardo)
 static const uint8_t CAN_CS_PIN  = 9;
 static const uint8_t CAN_INT_PIN = 8;
-static const uint32_t CAN_SPEED  = CAN_500KBPS;
+static const uint32_t CAN_SPEED  = CAN_100KBPS;//comfort bus
 
 MCP_CAN CAN(CAN_CS_PIN);
 
 // ---------------- IDs ----------------
-static const uint32_t ID_PDC_RESPONSE = 0x054B;
+static const uint32_t ID_PDC_DIST = 0x054B;
+static const uint32_t ID_PDC_STAT = 0x0545;
 
-static const uint32_t ID_REVERSE_DATA = 0x390;
-static const uint32_t ID_SPEED_DATA = 0x320;
+
+static const uint32_t ID_REVERSE_AND_SPEED_DATA = 0x351; //ggf: BO_ 1136 mBSG_Kombi: 8 Gateway_PQ35
+static const uint32_t ID_IGNITION_DATA = 0x575;
+
 
 
 // ---------------- Requests ----------------
-uint8_t msg_320[8] = {0,0,0,0,0,0,0,0};
-uint8_t msg_390[8] = {0,0,0x02,0x10,0,0,0,0}; 
-// Kein Anhänger
-// GK1_RueckfahrSch = 1 (Byte 2 - Bit1)
-// GK1_Rueckfahr = 1 (Byte 3 - Bit4)
+uint8_t msg_351[8] = {0x20,0,0,0,0,0,0,0};
+uint8_t msg_575[4] = {0x20,0,0,0};
 
 
 // ---------------- State ----------------
@@ -83,9 +83,6 @@ void setup() {
   phase1_start = millis();
   phase2_start = millis();
   phase3_start = millis();
-
-  calculateSpeed(0, msg_320);
-
 }
 
 // ---------------- Loop ----------------
@@ -95,12 +92,13 @@ void loop() {
   // -------- Phasenwechsel --------
   if (now - phase1_start >= 25) {
     phase1_start = now;
-    sendFrame(ID_SPEED_DATA, 8, msg_320);
+    
   }
 
 if (now - phase2_start >= 100) {
     phase2_start = now;  
-    sendFrame(ID_REVERSE_DATA, 8, msg_390);
+    sendFrame(ID_REVERSE_AND_SPEED_DATA, 8, msg_351);
+    sendFrame(ID_IGNITION_DATA, 8, msg_575);
   }
   // -------- RX --------
   if (can_irq) can_irq = false;
@@ -110,25 +108,11 @@ if (now - phase2_start >= 100) {
     uint8_t len;
     uint8_t buf[8];
     CAN.readMsgBuf(&rxId, &len, buf);
-
-    if (rxId == ID_PDC_RESPONSE) {
-        //printFrame("RX", rxId, len, buf);
+    printFrame("RX", rxId, len, buf);
+    if (rxId == ID_PDC_DIST || rxId == ID_PDC_STAT) {    
       decodeData(rxId, len, buf);
-    }
+    }    
   }
-}
-
-void calculateSpeed(float speed_kmh, uint8_t *d) {
-  
-  uint16_t raw_speed = (uint16_t)(speed_kmh / 0.32); // = 31
-
-  //SG_ Angezeigte_Geschwindigkeit : 46|10@1+
-  //
-  // Startbit 46:
-  //  - Byte5 Bit6–7  -> obere 2 Bits
-  //  - Byte6 Bit0–7  -> untere 8 Bits
-  d[5] |= (raw_speed & 0x03) << 6;   // Bits 0–1 -> Byte5 Bit6–7
-  d[6] |= (raw_speed >> 2) & 0xFF;   // Bits 2–9 -> Byte6
 }
 
 static inline bool getBit(const byte* d, uint8_t byteIndex, uint8_t bitIndex) {
@@ -144,19 +128,74 @@ static inline uint8_t getBits(const byte* d, uint8_t byteIndex, uint8_t lsbBit, 
 void decodeData (uint32_t id, uint8_t len,  uint8_t *d){
    
   if (id == ID_PDC_RESPONSE && len >= 8) {
-    uint8_t hinten_links  = d[2]; // Byte 3
-    uint8_t hinten_rechts = d[3]; // Byte 4
-    uint8_t hinten_mitte_links  = d[6]; // Byte 7
-    uint8_t hinten_mitte_rechts = d[7]; // Byte 8;
+    
+  Serial.println("==== mParkhilfe_5 ====");
 
-    Serial.print("HL: ");
-    printHex2(hinten_links);
-    Serial.print(" HML: ");
-    printHex2(hinten_mitte_links);
-    Serial.print(" HMR: ");
-    printHex2(hinten_mitte_rechts);
-    Serial.print(" HR: ");
-    printHex2(hinten_rechts);
-    Serial.println("");
+  // VORNE (links -> rechts)
+  Serial.print("Vorne : ");
+  Serial.print(d[0]);  Serial.print("  |  ");   // VL
+  Serial.print(d[4]);  Serial.print("  |  ");   // VML
+  Serial.print(d[5]);  Serial.print("  |  ");   // VMR
+  Serial.print(d[1]);  Serial.println("");   // VR
+
+  // HINTEN (links -> rechts)
+  Serial.print("Hinten: ");
+  Serial.print(d[2]);  Serial.print("  |  ");   // HL
+  Serial.print(d[6]);  Serial.print("  |  ");   // HML
+  Serial.print(d[7]);  Serial.print("  |  ");   // HMR
+  Serial.print(d[3]);  Serial.println("");   // HR
+
+  Serial.println("======================");
+  Serial.println();
   }
+
+  if (id == ID_PDC_STAT && len >= 8) {
+    Serial.println("========== mParkhilfe_3 ==========");
+
+  // -------- Byte 0 --------
+  Serial.println("-- Systemstatus --");
+  Serial.print("System codiert: "); Serial.println((d[0] & 0x01) ? "JA" : "NEIN");
+  Serial.print("Anlage defekt: "); Serial.println((d[0] & 0x02) ? "JA" : "NEIN");
+  Serial.print("Taster aktiviert: "); Serial.println((d[0] & 0x04) ? "JA" : "NEIN");
+  Serial.print("Rueckwaertsgang aktiv: "); Serial.println((d[0] & 0x08) ? "JA" : "NEIN");
+  Serial.print("Geschwindigkeit aus: "); Serial.println((d[0] & 0x10) ? "JA" : "NEIN");
+  Serial.print("Tongeber vorne defekt: "); Serial.println((d[0] & 0x20) ? "JA" : "NEIN");
+  Serial.print("Tongeber hinten defekt: "); Serial.println((d[0] & 0x40) ? "JA" : "NEIN");
+  Serial.print("Audioabsenkung Anforderung: "); Serial.println((d[0] & 0x80) ? "JA" : "NEIN");
+
+  // -------- Byte 1 --------
+  Serial.println("\n-- Zusatzstatus --");
+  Serial.print("Eis aus: "); Serial.println((d[1] & 0x01) ? "JA" : "NEIN");
+  Serial.print("Schlechtweg aus: "); Serial.println((d[1] & 0x02) ? "JA" : "NEIN");
+  Serial.print("KD Fehler: "); Serial.println((d[1] & 0x80) ? "JA" : "NEIN");
+
+  // -------- Byte 2 --------
+  Serial.println("\n-- Sensorstatus Hinten --");
+  Serial.print("HLL defekt: "); Serial.println((d[2] & 0x01) ? "JA" : "NEIN");
+  Serial.print("HLM defekt: "); Serial.println((d[2] & 0x02) ? "JA" : "NEIN");
+  Serial.print("HRM defekt: "); Serial.println((d[2] & 0x04) ? "JA" : "NEIN");
+  Serial.print("HRR defekt: "); Serial.println((d[2] & 0x08) ? "JA" : "NEIN");
+
+  Serial.println("\n-- Sensorstatus Vorne --");
+  Serial.print("VLL defekt: "); Serial.println((d[2] & 0x10) ? "JA" : "NEIN");
+  Serial.print("VLM defekt: "); Serial.println((d[2] & 0x20) ? "JA" : "NEIN");
+  Serial.print("VRM defekt: "); Serial.println((d[2] & 0x40) ? "JA" : "NEIN");
+  Serial.print("VRR defekt: "); Serial.println((d[2] & 0x80) ? "JA" : "NEIN");
+
+  // -------- Byte 3 --------
+  Serial.println("\n-- Betriebsstatus --");
+  Serial.print("Bereit: "); Serial.println((d[3] & 0x01) ? "JA" : "NEIN");
+
+  uint8_t bereich_vo = (d[3] >> 2) & 0x03;
+  uint8_t bereich_hi = (d[3] >> 4) & 0x03;
+
+  Serial.print("Bereich vorne: "); Serial.println(bereich_vo);
+  Serial.print("Bereich hinten: "); Serial.println(bereich_hi);
+
+  Serial.print("Akustik aus: "); Serial.println((d[3] & 0x40) ? "JA" : "NEIN");
+  Serial.print("Aufbau erkannt: "); Serial.println((d[3] & 0x80) ? "JA" : "NEIN");
+
+  Serial.println("==================================\n");
+  }
+    
 }
